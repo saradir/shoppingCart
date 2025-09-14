925ec9796005fddeaa369e1d1e3677961254e6f6
saradir <120840462+saradir@users.noreply.github.com>
2025-09-15 01:27:15 +0300

    WIP: added CartItem layout and basic styling

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styles from '../styles/CartItem.module.css';
 
 function CartItem({key, id, product, qty, updateCartItem}){
             const [localQty, setLocalQty] = useState(qty);
@@ -7,17 +8,24 @@ function CartItem({key, id, product, qty, updateCartItem}){
             
 
             return (
-              <div key={key} className="cart-item">
-                    <div className='product-name'>{product.title}</div>
-                    <button onClick={increase}> + </button>
-                    <input type = 'number'   value={localQty} onChange={(e) => setLocalQty(Number(e.target.value))}></input>
-                    <button onClick={decrease}> - </button>
+              <div key={key} className={styles.cartItem}>
+                <div className={styles.cartItemImage}>
+                  <img src={product.image}></img>
+                </div>
+                <div className={styles.itemRight}>
+                    <div className={styles.productName} title={product.title}>{product.title}</div>
+                    <div className={styles.qtyControl}>
+                      <button onClick={increase}> + </button>
+                      <input type = 'number'   value={localQty} onChange={(e) => setLocalQty(Number(e.target.value))}></input>
+                      <button onClick={decrease}> - </button>
+                    </div>
                     <span>Total: {product.price * qty}</span>
                     <button onClick={() => updateCartItem(id, 0)}>Remove</button>
 
                      {localQty !== qty && (
                         <button onClick={() => updateCartItem(id, localQty)}>Update</button>
                     )}
+                </div> 
               </div>
                 )
 
