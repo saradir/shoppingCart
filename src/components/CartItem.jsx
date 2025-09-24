2f8a198618a5036f6ec859fc5ccc2a347f320714
saradir <120840462+saradir@users.noreply.github.com>
2025-09-25 01:39:06 +0300

    restyle cartitem and fix update button flicker on update outside pane

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,33 +1,55 @@
-import { useState } from "react";
+import { useState, useLayoutEffect } from "react";
 import styles from '../styles/CartItem.module.css';
+import { isVisible } from "@testing-library/user-event/dist/cjs/utils/index.js";
 
 function CartItem({key, id, product, qty, updateCartItem}){
             const [localQty, setLocalQty] = useState(qty);
             const increase = () => setLocalQty( (q) => q + 1);
             const decrease = () => setLocalQty( (q) => q - 1);
             
+  // sync immediately before paint
+  useLayoutEffect(() => {
+    setLocalQty(qty);
+  }, [qty]);
 
-            return (
-              <div key={key} className={styles.cartItem}>
-                <div className={styles.cartItemImage}>
-                  <img src={product.image}></img>
-                </div>
-                <div className={styles.itemRight}>
-                    <div className={styles.productName} title={product.title}>{product.title}</div>
-                    <div className={styles.qtyControl}>
-                      <button onClick={increase}> + </button>
-                      <input type = 'number'   value={localQty} onChange={(e) => setLocalQty(Number(e.target.value))}></input>
-                      <button onClick={decrease}> - </button>
-                    </div>
-                    <span>Total: {product.price * qty}</span>
-                    <button onClick={() => updateCartItem(id, 0)}>Remove</button>
+           return (
+  <div className={styles.cartItem}>
+    <button 
+      className={styles.removeBtn} 
+      onClick={() => updateCartItem(id, 0)}
+    >
+      ×
+    </button>
 
-                     {localQty !== qty && (
-                        <button onClick={() => updateCartItem(id, localQty)}>Update</button>
-                    )}
-                </div> 
-              </div>
-                )
+    <div className={styles.cartItemImage}>
+      <img src={product.image} alt={product.title} />
+    </div>
 
+    <div className={styles.itemRight}>
+      <div className={styles.productName} title={product.title}>
+        {product.title}
+      </div>
+
+      <div className={styles.qtyControl}>
+        <button onClick={increase}> + </button>
+        <input 
+          type="number" 
+          value={localQty} 
+          onChange={(e) => setLocalQty(Number(e.target.value))} 
+        />
+        <button onClick={decrease}> - </button>
+      </div>
+
+      <span>Total: ${(product.price * qty).toFixed(2)}</span>
+
+      <button 
+        onClick={() => updateCartItem(id, localQty)}   
+        className={`${styles.updateBtn} ${qty !== localQty ? styles.active : ""}`}
+      >
+        Update
+      </button>
+    </div> 
+  </div>
+);
 }
 export default CartItem;
