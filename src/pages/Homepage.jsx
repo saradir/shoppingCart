f256944c9fc3cdfb73f4878822f56a7bccc8cd8b
saradir <120840462+saradir@users.noreply.github.com>
2025-09-28 00:24:24 +0300

    add some content to homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,20 @@
+import styles from '../styles/Homepage.module.css';
+import { Link } from "react-router-dom";
+
 function Homepage(){
-    return <div>Home Page</div>
+    return <div className={styles.mainContainer}>
+        <h1>Welcome to the Random Store</h1>
+        <p>
+            Step into a world where everything is a little unexpected. <br></br>
+            From stylish clothing to curious gadgets, our shelves are stocked <br></br>
+            with all sorts of random treasures waiting to be discovered. <br></br>
+        </p>
+        <p> 
+            Click
+            <Link to="/shop" className={styles.shopLink}> here </Link> 
+            to start your journey
+        </p>
+        </div>
 }
 
 export default Homepage;
