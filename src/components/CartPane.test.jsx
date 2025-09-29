104b5f412a49be21945d725877e1ef605da76d53
saradir <120840462+saradir@users.noreply.github.com>
2025-09-29 20:58:07 +0300

    add new tests to CartPane

--- src/components/CartPane.test.jsx
+++ src/components/CartPane.test.jsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi } from 'vitest';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, getByRole } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import CartPane from './CartPane';
 import { MemoryRouter } from "react-router-dom";
@@ -34,5 +34,35 @@ describe('test CartPane', () => {
     fireEvent.click(removeBtn);
 
     expect(updateCart).toBeCalledWith('1', 0);
+  });
+
+  it('button is disabled when cart is empty', ()=>{
+    const catalogue = [{ id: '1', title: 'Test Product', price: 10 }];
+
+    render(
+      <MemoryRouter>
+        <CartPane cart={{}} catalogue={catalogue} updateCartItem={() => {}} />
+      </MemoryRouter>
+    );
+
+    const checkoutBtn = screen.getByRole('button', {name: /checkout/i});
+    expect(checkoutBtn).toBeDisabled();
+  });
+
+  it('navigates to checkout when clicking on checkout button in cart', async () =>{
+    const cart = { 1: 2 };
+    const catalogue = [{ id: '1', title: 'Test Product', price: 10 }];
+    const updateCart = vi.fn();
+    render(
+      <MemoryRouter>
+      <CartPane cart={cart} catalogue={catalogue} updateCartItem={updateCart} />
+      </MemoryRouter>
+    );
+
+    const checkoutBtn = screen.getByRole('button', {name: /checkout/i});
+    await userEvent.click(checkoutBtn);
+
+    expect(checkoutBtn).not.toBeDisabled();
+
   });
 });
